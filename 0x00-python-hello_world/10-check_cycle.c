dd976a55a947727ce85ae9fb24b57610a4d7958f
luismvargasg <1610@holbertonschool.com>
2020-04-28 06:25:12 +0000

    adding task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * check_cycle - function that checks if a singly linked list has a cycle in it
+ * @list: Given list to find if it has a cycle.
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle.
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *hare = list, *tortoise = list;
+
+	while (hare != '\0' && tortoise != '\0')
+	{
+		hare = hare->next->next;
+		tortoise = tortoise->next;
+		if (hare == tortoise)
+			return (1);
+	}
+	return (0);
+}
