58748d3626d159bdf3ce4ad04465137f5980c0e6
luismvargasg <1610@holbertonschool.com>
2020-05-06 00:49:06 +0000

    fixing None if string is empty

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    tuple = len(sentence), sentence[0]
+    if len(sentence) > 0:
+        tuple = len(sentence), sentence[0]
+    else:
+        tuple = len(sentence), None
     return tuple
