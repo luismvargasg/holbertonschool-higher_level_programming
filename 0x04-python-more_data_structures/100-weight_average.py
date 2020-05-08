79d18fde6d61fa0cf6bc04e48343e496856f9f48
luismvargasg <1610@holbertonschool.com>
2020-05-08 04:24:14 +0000

    adding task 100

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if len(my_list) == 0:
-        return None
-    mul_res = 0
-    weight_sum = 0
-    for score, weight in my_list:
-        mul_res += score * weight
-        weight_sum += weight
-    return mul_res / weight_sum
+    if my_list:
+        mul_res = 0
+        weight_sum = 0
+        for score, weight in my_list:
+            mul_res += score * weight
+            weight_sum += weight
+        return mul_res / weight_sum
+    return 0
