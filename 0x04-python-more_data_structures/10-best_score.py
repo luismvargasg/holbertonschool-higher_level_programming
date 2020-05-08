16517b113f3a833fcff623ad9d52d12ad20d6c3f
luismvargasg <1610@holbertonschool.com>
2020-05-08 00:35:28 +0000

    adding task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        winner = max(list(a_dictionary))
+        winner = max(a_dictionary, key=a_dictionary.get)
         return winner
-    else:
-        return None
