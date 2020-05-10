b042a4b76da5d497d3c5a6d51c3fa9c1d9edadf5
luismvargasg <1610@holbertonschool.com>
2020-05-10 00:59:04 +0000

    fixing empty string case

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is not None or type(roman_string) is str:
-        r_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
-                  'M': 1000}
-        integer = 0
-        for i in range(len(roman_string)):
-            if roman_string[i] in r_dict:
-                integer += r_dict[roman_string[i]]
-                if i > 0 and r_dict[roman_string[i]] > r_dict[roman_string[i - 1]]:
-                    integer -= 2 * r_dict[roman_string[i - 1]]
-            else:
-                return 0
-        return integer
-    return 0
+    if roman_string is None or type(roman_string) is not str:
+        return 0
+    r_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
+              'M': 1000}
+    integer = 0
+    for i in range(len(roman_string)):
+        integer += r_dict[roman_string[i]]
+        if i > 0 and r_dict[roman_string[i]] > r_dict[roman_string[i - 1]]:
+            integer -= 2 * r_dict[roman_string[i - 1]]
+    return integer
