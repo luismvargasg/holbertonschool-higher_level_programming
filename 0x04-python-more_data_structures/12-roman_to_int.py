f58f3c040135f968f06bea289b8c4064e83529d7
luismvargasg <1610@holbertonschool.com>
2020-05-10 00:33:57 +0000

    adding task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string or type(roman_string) is str:
+        r_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
+                  'M': 1000}
+        integer = 0
+        for i in range(len(roman_string)):
+            integer += r_dict[roman_string[i]]
+            if i > 0 and r_dict[roman_string[i]] > r_dict[roman_string[i - 1]]:
+                integer -= 2 * r_dict[roman_string[i - 1]]
+        return integer
