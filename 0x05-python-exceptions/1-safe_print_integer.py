be4ad69088ec527f61e81215e1b72ac645736935
luismvargasg <1610@holbertonschool.com>
2020-05-18 17:57:35 +0000

    fixing pep8 error

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,6 +2,6 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-        return True # if value has been printed (it means is an integer)
+        return True  # if value has been printed (it means is an integer)
     except:
-        return False # if value is not an integer.
+        return False  # if value is not an integer.
