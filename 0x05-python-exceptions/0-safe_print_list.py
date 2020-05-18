706a0f5c8da9c0220b338b099df70130c4b45dbc
luismvargasg <1610@holbertonschool.com>
2020-05-18 17:41:51 +0000

    adding task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     elem = 0
     try:
         for i in range(x):
-            print("{}".format(my_list[i]), end="")
+            print("{:d}".format(my_list[i]), end="")
             elem += 1
         print()
     except:
