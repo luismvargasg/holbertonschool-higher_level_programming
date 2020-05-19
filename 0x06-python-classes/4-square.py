807863d9ebc1c014675a7da4474da9eb28149236
luismvargasg <1610@holbertonschool.com>
2020-05-19 21:37:16 +0000

    fixing documentation

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""creating a class"""
+
+
+class Square:
+    """Defines a square with one property: a writable size.
+
+    Attributes:
+        size: (:obj: `int`, optional): private instance.
+        area: (:obj: `int`): public instance method, returns square area.
+
+    Raises:
+        TypeError: An exception if parameter size is not an integer.
+        ValueError: An exception if parameter size is less than 0.
+    """
+
+    def __init__(self, size=0):
+        # if type(size) is not int:
+        #    raise TypeError("size must be an integer")
+        # elif size < 0:
+        #    raise ValueError("size must be >= 0")
+        # else:
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return self.__size ** 2
