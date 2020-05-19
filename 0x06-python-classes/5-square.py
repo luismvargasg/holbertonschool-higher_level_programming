ae044a1d0abec732e392c7693042a8390048daa0
luismvargasg <1610@holbertonschool.com>
2020-05-19 21:40:03 +0000

    fixing documentation

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -15,11 +15,6 @@ class Square:
     """
 
     def __init__(self, size=0):
-        # if type(size) is not int:
-        #    raise TypeError("size must be an integer")
-        # elif size < 0:
-        #    raise ValueError("size must be >= 0")
-        # else:
         self.__size = size
 
     @property
