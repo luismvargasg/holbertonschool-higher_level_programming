f06d53436f148dca8665a5914e0f3da1df60dc54
luismvargasg <1610@holbertonschool.com>
2020-05-20 03:46:42 +0000

    fixing pep8

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -12,10 +12,10 @@ class MagicClass:
             raise TypeError('radius must be a number')
         self.__radius = radius
 
-
+    """comment"""
     def area(self, radius):
-       return self.__radius ** 2 * math.pi
+        return self.__radius ** 2 * math.pi
 
-
+    """comment"""
     def circumference(self, radius):
         return 2 * math.pi * self.__radius
