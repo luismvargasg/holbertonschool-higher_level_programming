558d2fff9656489dc9a64d3e5aac4f47f47a0838
luismvargasg <1610@holbertonschool.com>
2020-05-23 20:27:26 +0000

    adding case: multiple spaces

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -5,13 +5,10 @@
 def text_indentation(text):
     """prints a text with 2 new lines after each
     of these characters: ., ? and :.
-
     Args:
         text: must be a string.
-
     Returns:
          nothing.
-
     Raises:
         TypeError: if text is not a string.
     """
@@ -22,10 +19,13 @@ def text_indentation(text):
     for i in text:
         if flag == 1 and i == " ":
             string += ""
-            flag = 0
+            flag == 2
+        elif flag == 2 and i == " ":
+            string += ""
         elif i in [".", "?", ":"]:
             flag = 1
             string += i + "\n\n"
         else:
             string += i
+            flag = 0
     print(string, end="")
