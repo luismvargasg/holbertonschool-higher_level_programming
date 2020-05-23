d41da71fc86fa97320165434c5755000ca7cc78b
luismvargasg <1610@holbertonschool.com>
2020-05-23 03:47:32 +0000

    adding task 5

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """unittest class for max_integer
+    function
+    """
+    def test_max_pos(self):
+        """Test max integer in positive numbers"""
+        test_list = [1, 2, 3, 4]
+        self.assertEqual(max_integer(test_list), max(test_list))
+        test_list = [99, 2540, 9999, float("inf")]
+        self.assertEqual(max_integer(test_list), max(test_list))
+
+    def test_max_neg(self):
+        """Test max integer in negative numbers"""
+        test_list = [-1, -2, -3, -4]
+        self.assertEqual(max_integer(test_list), max(test_list))
+        test_list = [-99, -2540, -9999, float("-inf")]
+        self.assertEqual(max_integer(test_list), max(test_list))
+
+    def test_max_both(self):
+        """Test max integer in positive and negative numbers"""
+        test_list = [-1, 2, -3, 4]
+        self.assertEqual(max_integer(test_list), max(test_list))
+        test_list = [99, -2540, -9999, 5000]
+        self.assertEqual(max_integer(test_list), max(test_list))
+
+    def test_max_raise(self):
+        """Test max integer for raising TypeErrors"""
+        test_list = [1, 2, 3, "a"]
+        self.assertRaises(TypeError, max_integer, test_list)
+        test_list = []
+        self.assertRaises(ValueError, max_integer, test_list)
+
+    def test_max_none(self):
+        """Test max void parameters"""
+        self.assertIsNone(max_integer(), msg=None)
