a36c0b27a9e1c5a7dc1692db218837ca1e110eb6
luismvargasg <1610@holbertonschool.com>
2020-05-25 23:06:28 +0000

    adding documentation in task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -24,6 +24,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
+        """setter for attribute width"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -38,6 +39,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
+        """setter for attribute height"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
         elif value < 0:
