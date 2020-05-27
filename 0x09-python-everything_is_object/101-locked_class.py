680b1a38743a6b0c22ecc4e1ab8055d34f104ea4
luismvargasg <1610@holbertonschool.com>
2020-05-27 01:00:45 +0000

    adding task 31

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module to create a LockedClass"""
+
+
+class LockedClass:
+    """defines a locked clas"""
+    __slots__ = ['first_name']
+
+    def __init__(self):
+        pass
