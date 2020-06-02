5b89fb26baeb1af6a1af66fd52d18a6a63e5a9c2
luismvargasg <1610@holbertonschool.com>
2020-06-02 22:16:14 +0000

    adding task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module to create a class"""
+
+
+class BaseGeometry:
+    """defining an empty class"""
+    def area(self):
+        """public instance method"""
+        raise Exception("area() is not implemented")
