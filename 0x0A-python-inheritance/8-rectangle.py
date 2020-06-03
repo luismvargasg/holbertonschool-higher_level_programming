f9cce01adc4cb5723f066ca8aae78099620f519e
luismvargasg <1610@holbertonschool.com>
2020-06-03 00:15:21 +0000

    changing import

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,19 +2,7 @@
 """Module to create a class"""
 
 
-class BaseGeometry:
-    """defining an empty class"""
-    def area(self):
-        """public instance method"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Public instance method to validae received value"""
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-        return value
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
