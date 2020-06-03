844ecead16c629a1dd701b8a8b291b9c74fe344e
luismvargasg <1610@holbertonschool.com>
2020-06-03 00:25:52 +0000

    adding task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Module to create a class"""
+
+
+class BaseGeometry:
+    """defining an empty class"""
+    def area(self):
+        """public instance method"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Public instance method to validae received value"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+        return value
+
+
+class Rectangle(BaseGeometry):
+    """defining a class that inherits from BaseGeometry"""
+    def __init__(self, width, height):
+        """instatiation of class Rectangle"""
+        self.__width = self.integer_validator("width", width)
+        self.__height = self.integer_validator("height", height)
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
+
+
+class Square(Rectangle):
+    """defining a class that inherits from Rectangle"""
+    def __init__(self, size):
+        self.__size = self.integer_validator("size", size)
+
+    def area(self):
+        """method to find the area of Square"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        return "[Rectangle] " + str(self.__size) + "/" + str(self.__size)
