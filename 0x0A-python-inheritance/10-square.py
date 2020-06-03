43c33babae9036c7629345c1af4ecbfa148e5cb9
luismvargasg <1610@holbertonschool.com>
2020-06-03 00:33:56 +0000

    changing import method

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -2,33 +2,7 @@
 """Module to create a class"""
 
 
-class BaseGeometry:
-    """defining an empty class"""
-    def area(self):
-        """public instance method"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Public instance method to validae received value"""
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-        return value
-
-
-class Rectangle(BaseGeometry):
-    """defining a class that inherits from BaseGeometry"""
-    def __init__(self, width, height):
-        """instatiation of class Rectangle"""
-        self.__width = self.integer_validator("width", width)
-        self.__height = self.integer_validator("height", height)
-
-    def area(self):
-        return self.__width * self.__height
-
-    def __str__(self):
-        return "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
@@ -41,4 +15,5 @@ class Square(Rectangle):
         return self.__size * self.__size
 
     def __str__(self):
+        """returning the string representation of the object"""
         return "[Rectangle] " + str(self.__size) + "/" + str(self.__size)
