f750d5a779f2a49dbfcebe15763e6c2270b08805
luismvargasg <1610@holbertonschool.com>
2020-06-03 22:13:20 +0000

    adding task 2

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module to read n lines text file"""
+
+
+def read_lines(filename="", nb_lines=0):
+    """function that reads n lines of a text file (UTF8)
+       and prints it to stdout.
+
+    Args:
+        filename: text file to read.
+        nb_lines:
+    """
+    with open(filename) as myFile:
+       lines = myFile.readlines()
+    if nb_lines <= 0 or nb_lines >= len(lines):
+        nb_lines = len(lines)
+    for i in range(nb_lines):
+        print(lines[i], end="")
