297e5a09ca1064b11daa83c8297cb81c669b34bd
luismvargasg <1610@holbertonschool.com>
2020-06-06 04:16:32 +0000

    adding task 15

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module to def a function that inserts a line"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """function that inserts a line of text to a file, after each
+       line containing a specific string.
+
+    Args:
+        filename: given file to add the line.
+        search_string: given string to look into the file.
+        new_string: string to add after the matching result.
+    """
+    update_file = ""
+    with open(filename, mode="r+") as myFile:
+        for line in myFile:
+            update_file += line
+            if search_string in line:
+                update_file += new_string
+        myFile.write(update_file)
