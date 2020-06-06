59a02af3e09b9ec5e4b721649a57eb7dedd3e5df
luismvargasg <1610@holbertonschool.com>
2020-06-06 04:34:09 +0000

    backup task 6

---
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""module to matrices multiplication"""
+
+
+def matrix_mul(m_a, m_b):
+    """function that multiplies 2 matrices.
+
+    Args:
+        m_a: first matrix, must be an list of lists of integers or floats.
+        m_a: second matrix, must be an list of lists of integers or floats.
+
+    Returns:
+        new matrix with the result of the multiplication.
+
+    Raises:
+        TypeError: if m_a or m_b is not a list.
+        TypeError: if m_a or m_b is not a list of lists.
+        ValueError: if m_a or m_b is empty / = [] or = [[]]
+        TypeError: if one element of those list is not and int or float.
+        TypeError: if m_a or m_b is not a rectangle / != row size.
+        ValueError: if m_a or m_b can´t ve multiplied.
+    """
