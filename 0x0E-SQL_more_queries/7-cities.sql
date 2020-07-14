0993a7d4cb091e0e2f3b404a954faf67b18ee841
luismvargasg <1610@holbertonschool.com>
2020-07-14 21:23:03 +0000

    Fixing Charlie's Error on FOREIGN

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,11 +3,11 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
-    id INT UNIQUE NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (id),
+    id INT NOT NULL AUTO_INCREMENT,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id),
 
-    FOREING KEY (state_id)
+    FOREIGN KEY (state_id)
         REFERENCES hbtn_0d_usa.states(id)
 );
