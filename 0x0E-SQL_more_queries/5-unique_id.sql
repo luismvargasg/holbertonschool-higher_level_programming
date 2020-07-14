5857f021091b4464f8d7209766089c4517524b14
luismvargasg <1610@holbertonschool.com>
2020-07-14 20:23:04 +0000

    adding task 5

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,6 @@
 -- Script that creates the table unique_id on your MySQL server
 -- where id must be a unique value set to 1 by dafault.
-CREATE IF NOT EXISTS unique_id (
+CREATE TABLE IF NOT EXISTS unique_id (
     id INT PRIMARY KEY DEFAULT 1,
     name VARCHAR(256)
 );
