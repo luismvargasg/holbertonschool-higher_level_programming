63ac84da97a6669f545f49065f966f7bd827e0a2
luismvargasg <1610@holbertonschool.com>
2020-08-18 22:48:16 +0000

    adding curl GET

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL and displays all HTTP methods the server will accept
-curl -sX "$1" -H "X-HolbertonSchool-User-Id: 98"
+curl -sX GET "$1" -H "X-HolbertonSchool-User-Id: 98"
