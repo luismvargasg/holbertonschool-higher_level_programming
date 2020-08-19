16913ef0c4ea9266ed20cf26358e4f5faaf1af85
luismvargasg <1610@holbertonschool.com>
2020-08-19 00:38:44 +0000

    adding task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Function that finds a peak in a list of unsorted integers."""
+
+
+def find_peak(list_of_integers):
+    """Return the peak if there's one or None is the list is empty"""
+    if list_of_integers:
+        return max(list_of_integers)
+    else:
+        return None
