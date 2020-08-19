ebb245ba0e756c6389e1de003c6d03831a1cef38
luismvargasg <1610@holbertonschool.com>
2020-08-19 00:48:15 +0000

    changing method to sort

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,6 +5,7 @@
 def find_peak(list_of_integers):
     """Return the peak if there's one or None is the list is empty"""
     if list_of_integers:
-        return max(list_of_integers)
+        list_of_integers.sort()
+        return list_of_integers[-1]
     else:
         return None
