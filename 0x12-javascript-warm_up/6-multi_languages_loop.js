0caf3ffdd6b1b6c1d1679e1878c7fd706efc7aa1
luismvargasg <1610@holbertonschool.com>
2020-09-15 22:29:07 +0000

    missing semicolon

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing']
+const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (let step = 0; step < 3; step++) {
   console.log(myArray[step]);
 }
