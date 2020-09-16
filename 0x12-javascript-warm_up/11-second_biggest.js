aa1dc7b08c9a356cdb120c6652a23dccf847ff91
luismvargasg <1610@holbertonschool.com>
2020-09-16 02:12:31 +0000

    changing sorting method

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 if (parseInt(process.argv[3])) {
   const res = process.argv;
   const newRes = res.slice(2);
-  newRes.sort();
+  newRes.sort((a, b) => a - b);
   console.log(newRes[newRes.length - 2]);
 } else {
   console.log('0');
