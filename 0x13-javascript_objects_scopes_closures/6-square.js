9da55ea975d1112f8982fee80183249f64b8c9f0
luismvargasg <1610@holbertonschool.com>
2020-09-21 20:18:16 +0000

    adding files for task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const square = require('./5-square');
+
+class Square extends square {
+  charPrint (c) {
+    for (let i = 0; i < this.height; i++) {
+      if (c) {
+        console.log(c.repeat(this.width));
+      } else {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+}
+
+module.exports = Square;
