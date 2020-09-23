ed45cf22247ab38c7e2d567ac1557c7e515bfacf
luismvargasg <1610@holbertonschool.com>
2020-09-23 00:45:21 +0000

    fixing logic

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,11 +2,11 @@
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/';
 const str = '18';
+let res = 0;
 request(url, function (err, response, body) {
   if (err) {
     console.log(err);
   } else {
-    let res = 0;
     for (const movie of JSON.parse(body).results) {
       for (const character of movie.characters) {
         if (character.includes(str)) {
